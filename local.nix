e79df646dec561e2a620f05e191dd4d8a93abbde
Keith Bauson <kwbauson@gmail.com>
2021-07-24 11:40:02 -0400

    add yarn stuff and initial migration

--- local.nix
+++ local.nix
@@ -1,17 +1,17 @@
-scope: with scope;
-let ruby = scope.ruby.withPackages (pkgs: with pkgs; [
+scope:
+let ruby = scope.ruby_3_0.withPackages (pkgs: with pkgs; [
   rails
   pg
   puma
   listen
-  rufo
-]);
-in
-[
+  rake
+]); in
+with scope; [
   (writeScriptBin "rails" ''
     #!/${ruby}/bin/ruby
     APP_PATH = File.expand_path('${source}/config/application', __dir__)
     require "rails/commands"
   '')
   ruby
+  rufo
 ]
